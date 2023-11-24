44ce80c363acf77b57b6229a8a5e1596ac4ffdef
9wynneth <gisviandhy@student.ciputra.ac.id>
2023-11-24 08:06:42 +0700

    Initial Commit

---
+++ coba_afl3/coba_afl3App.swift
@@ -0,0 +1,17 @@
+//
+//  coba_afl3App.swift
+//  coba_afl3
+//
+//  Created by MacBook Pro on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct coba_afl3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
